d000e324d2f55183211d02e2121e8026c2c22457
Eric Davis <edavis@littlestreamsoftware.com>
2011-08-29 15:59:40 -0700

    [#4430] Implemented Project#backlog_time

--- lib/chiliproject_work_reports/patches/project_patch.rb
+++ lib/chiliproject_work_reports/patches/project_patch.rb
@@ -14,9 +14,52 @@ module ChiliprojectWorkReports
       end
 
       module InstanceMethods
+        # Calculate the average number of days issues have spent in the Backlog status
+        #
+        # (Backlog status is provided from the Kanban configuration)
         def backlog_time
           return 0 if issues.count == 0
           raise ChiliprojectWorkReports::KanbanNotConfiguredError unless kanban_backlog_configured?
+
+          backlog_issue_status = IssueStatus.find(Setting.plugin_redmine_kanban["panes"]["backlog"]["status"])
+          
+          backlog_durations = issues.inject([]) do |time_spans, issue|
+            currently_in_backlog = issue.status == backlog_issue_status
+
+            journals_in_reverse = issue.journals.all(:order => 'version DESC',
+                                                     :conditions => ["#{Journal.table_name}.created_at > ?", 30.days.ago])
+
+            # Looking for From other to backlog: "status_id" => [other, backlog]
+            last_time_changed_to_backlog = journals_in_reverse.detect {|journal|
+              journal.changes["status_id"].present? &&
+              journal.changes["status_id"].last == backlog_issue_status.id
+            }
+            unless currently_in_backlog
+              # Looking for From backlog to other: "status_id" => [backlog, other]
+              last_time_changed_from_backlog = journals_in_reverse.detect {|journal|
+                journal.changes["status_id"].present? &&
+                journal.changes["status_id"].first == backlog_issue_status.id
+              }
+            end
+
+            time_spans << case
+                          when currently_in_backlog && last_time_changed_to_backlog.present?
+                            Date.today - last_time_changed_to_backlog.created_at.to_date
+                          when last_time_changed_to_backlog.present? && last_time_changed_from_backlog.present?
+                            last_time_changed_from_backlog.created_at.to_date - last_time_changed_to_backlog.created_at.to_date
+                          else
+                            # Never entered backlog
+                            nil
+                          end
+            time_spans
+          end
+
+          # Toss issues that never entered backlog
+          backlog_durations.compact!
+          total_time = backlog_durations.sum
+          number_of_issues = backlog_durations.length
+          return 0 if number_of_issues == 0
+          return (total_time / number_of_issues)
         end
 
         private
--- test/unit/lib/chiliproject_work_reports/patches/project_patch_test.rb
+++ test/unit/lib/chiliproject_work_reports/patches/project_patch_test.rb
@@ -6,11 +6,43 @@ class ChiliprojectWorkReports::Patches::ProjectTest < ActionController::TestCase
       setup do
         configure_kanban_plugin
         @project = Project.generate!
-        @issue1 = Issue.generate_for_project!(@project)
+        @other_status = IssueStatus.generate!
+        Timecop.freeze(60.days.ago) do
+          @issue1 = Issue.generate_for_project!(@project, :subject => 'Over 40')
+          @issue2 = Issue.generate_for_project!(@project, :subject => 'Changed')
+          @issue3 = Issue.generate_for_project!(@project, :subject => 'Still backlog')
+        end
+        # 40 days ago #1 was set to backlog
+        Timecop.freeze(40.days.ago) do
+          @issue1.status = backlog_issue_status
+          assert @issue1.save
+        end
+        # 20 days ago #2 and #3 were set to backlog
+        Timecop.freeze(20.days.ago) do
+          @issue2.status = backlog_issue_status
+          assert @issue2.save
+          @issue3.status = backlog_issue_status
+          assert @issue3.save
+        end
+        # 10 days ago #1 was set to a different status
+        Timecop.freeze(10.days.ago) do
+          @issue1.status = @other_status
+          assert @issue1.save
+        end
+        # 5 days ago #2 was changed
+        Timecop.freeze(5.days.ago) do
+          @issue2.status = @other_status
+          assert @issue2.save
+        end
+        Timecop.return
+        # End results:
+        #  #1 => 40-10 = 30 days, started backlog 40 days ago
+        #  #2 => 20-5 = 15 days, started backlog 20 days ago
+        #  #3 => 20-0 = 20 days (still set), started backlog 20 days ago
       end
       
       should "return 0 if there are no issues" do
-        @issue1.destroy
+        @project.issues.destroy_all
         assert @project.issues.empty?
         assert_equal 0, @project.backlog_time
       end
@@ -29,7 +61,11 @@ class ChiliprojectWorkReports::Patches::ProjectTest < ActionController::TestCase
         end
       end
       
-      should "return the average number of days an issue has been in the backlog for the past 30 days"
+      should "return the average number of days an issue has been in the backlog for the past 30 days" do
+        # Skips #1 (started backlog out of date range)
+        # (#2 = 15, #3 = 20) / 2 = 17.5
+        assert_equal 17.5, @project.backlog_time
+      end
     end
   end
 end
