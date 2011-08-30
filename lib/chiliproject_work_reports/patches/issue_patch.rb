4e0837c9f4d4bcc81258cdcf85ac4b5ccf50db2c
Eric Davis <edavis@littlestreamsoftware.com>
2011-08-30 09:58:19 -0700

    [#4430] Implement Issue#days_from_statuses

--- lib/chiliproject_work_reports/patches/issue_patch.rb
+++ lib/chiliproject_work_reports/patches/issue_patch.rb
@@ -56,6 +56,18 @@ module ChiliprojectWorkReports
           end
         end
 
+        def last_time_status_changed_to(status)
+          # Looking for From other to status: "status_id" => [other, status]
+          last_time_changed = last_30_days_of_journals_in_reverse.detect {|journal|
+            journal.changes["status_id"].present? &&
+            journal.changes["status_id"].last == status.id
+          }
+
+          if last_time_changed.present?
+            last_time_changed.created_at
+          end
+        end
+
         def days_in_backlog
           if last_time_changed_to_backlog.present? && last_time_changed_from_backlog.present?
             return last_time_changed_from_backlog.to_date - last_time_changed_to_backlog.to_date
@@ -64,8 +76,20 @@ module ChiliprojectWorkReports
           end
         end
 
-        def days_from_statuses(from, to)
-          0
+        # Calculates the number of days it took for an issue to change
+        # from one status to another.
+        #
+        # Not required to be linear:
+        #  A => B => C
+        #  days_from_statuses(A, C) would count B's time
+        def days_from_statuses(from_status, to_status)
+          start_day = last_time_status_changed_to(from_status)
+          end_day = last_time_status_changed_to(to_status)
+          if start_day.present? && end_day.present?
+            return end_day.to_date - start_day.to_date
+          else
+            nil
+          end
         end
       end
     end
