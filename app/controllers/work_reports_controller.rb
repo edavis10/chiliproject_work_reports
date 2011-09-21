0af2884824f31bfbe8188ab28ae847afc3cf7761
Eric Davis <edavis@littlestreamsoftware.com>
2011-09-21 16:18:42 -0700

    [#6580] Prototype a SVG graph using static data

--- app/controllers/work_reports_controller.rb
+++ app/controllers/work_reports_controller.rb
@@ -1,3 +1,5 @@
+require 'SVG/Graph/TimeSeries'
+
 class WorkReportsController < ApplicationController
   unloadable
   rescue_from ::ChiliprojectWorkReports::KanbanNotConfiguredError, :with => :kanban_not_configured
@@ -7,6 +9,71 @@ class WorkReportsController < ApplicationController
 
   def index
     @projects = get_projects_based_on_roll_up
+  end
+
+  def graph
+    @project = Project.find(params[:project_id])
+
+    fields = []
+    12.times {|m| fields << month_name(((Date.today.month - 1 - m) % 12) + 1)}
+
+    graph = SVG::Graph::TimeSeries.new({
+                                         :height => 300,
+                                         :width => 400,
+                                         :area_fill => true,
+                                         :min_y_value => 0,
+                                         :stagger_x_labels => true,
+                                         :x_label_format => "%Y-%m-%d"
+                                       })
+
+    # Stack by adding previous data items
+
+    data1 = {
+      "2011-01-01" => 100,
+      "2011-02-01" => 120,
+      "2011-03-01" => 130,
+      "2011-04-01" => 140
+    }
+    
+
+    graphing_data1 = data1.inject([]) do |acc, data_item|
+      acc << data_item.first # date
+      acc << data_item.second # value
+      acc
+    end
+
+    data2 = data1.dup
+
+    # Some raw data
+    {
+      "2011-01-01" => 100,
+      "2011-02-01" => 120,
+      "2011-03-01" => 150,
+      "2011-04-01" => 160
+    }.each do |key, value|
+      data2[key] ||= 0
+      data2[key] += value
+    end
+
+    graphing_data2 = data2.inject([]) do |acc, data_item|
+      acc << data_item.first # date
+      acc << data_item.second # value
+      acc
+    end
+
+    graph.add_data({
+                     :data => graphing_data1,
+                     :title => 'issues'
+                   })
+    
+    graph.add_data({
+                     :data => graphing_data2,
+                     :title => 'issues2'
+                   })
+    
+    
+    headers["Content-Type"] = "image/svg+xml"
+    send_data(graph.burn, :type => "image/svg+xml", :disposition => "inline")
   end
 
   protected
